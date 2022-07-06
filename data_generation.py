1bb9e0d3c2764f95986203833b82ee9ca1b35132
Mateusz Cegielski <cegielski1903@gmail.com>
2022-07-06 11:37:27 +0200

    Create data and samples

--- data_generation.py
+++ data_generation.py
@@ -6,15 +6,6 @@ import json
 if __name__ == "__main__":
     number_of_sample = 20
     C = 760000
-    # with open ("dataset.csv",'w') as file:
-    #     for i in range(number_of_sample):
-
-    #         P = C * (random.randint(10,200)/100); # Parameters P will have randomly from 10% to 200% of C value
-    #         torque = 3_000 * (random.randint(10, 200) / 100);  # torque unit is spins per minute !
-    #         data = [P, torque,]
-    #         writer = csv.writer(file)
-    #         writer.writerow(data)
-
 
     with open("dataset.csv", "w") as file:
         line = "force,torque\n"
