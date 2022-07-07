4dafe0f5cb399dad4501c13069361942c5db1fc9
ad50922 <daniel.augusto69@icloud.com>
2022-07-07 15:17:11 +0200

    Bearing, shaft, main modified

--- bearing.py
+++ bearing.py
@@ -1,27 +1,23 @@
+""""The model of bearing with required parameters to calculate it"""
+
 from statistics import mean
 
 
 class Bearing:
-
+    """The class of bearing with constructor and two methods"""
     def __init__(self, dynamic_load_capacity, id_bearing):
-        self.C = dynamic_load_capacity
-        self.id = id_bearing
-        self.L = []
+        self.c_attribute = dynamic_load_capacity
+        self.item_id = id_bearing
+        self.l_attribute = []
         self.result = []
 
     def calculate(self, force):
-        self.L.append(self.C / force)
-        print( f"WYnik : { self.C / force}")
-
-    def print_result(self):
-        print(f" The durability of bearing is equal to {self.L}.\n")
-        if {self.L} >= {self.allowed_durability}:
-            print(f"{self.L} is too high, the {self.name} bearing is broken.\n")
-        else:
-            print(f"{self.L} is optimal, the {self.name} bearing is able to work.\n")
+        """Calculating the durability of bearings"""
+        self.l_attribute.append(self.c_attribute / force)
 
     def creating_final_data(self):
-        min_value = min(self.L)
-        mean_value = mean(self.L)
-        max_value = max(self.L)
+        """Calculating statistics such as mean, minimum, maximum in one list"""
+        min_value = min(self.l_attribute)
+        mean_value = mean(self.l_attribute)
+        max_value = max(self.l_attribute)
         self.result = [min_value, mean_value, max_value]
