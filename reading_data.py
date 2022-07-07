b887fd860ea608139510476ea6bcac8f9a93d089
ad50922 <daniel.augusto69@icloud.com>
2022-07-07 09:19:21 +0200

    Reading data modified

--- reading_data.py
+++ reading_data.py
@@ -6,30 +6,24 @@ import json
 def reading_csv(path):
     """Reading data from csv files """
     with open(path, encoding="utf-8") as csv_file:
-        type(csv_file)
-        csvreader= csv.reader(csv_file)
-        header = []
+        csvreader = csv.reader(csv_file)
         header = next(csvreader)
         print(header)
         rows = []
         for row in csvreader:
             rows.append(row)
         print(rows)
+
+    for i in range(len(rows)):
+        for j in range(len(rows[0])):
+            rows[i][j] = float(rows[i][j])
+
     return rows
 
 
-def reading_json(path,):
+def reading_json(path):
     """Reading data from csv files """
     with open(path, encoding="utf-8") as json_file:
         data = json.load(json_file)
         print(json.dumps(data, indent=1))
     return data
-
-
-PATH_CSV = "C:/Users/mateusc/Documents/programy w Py/Rozgrzewka/src/dataset.csv"
-PATH_JSON = "C:/Users/mateusc/Documents/programy w Py/Rozgrzewka/src/data.json"
-
-test = reading_csv(PATH_CSV)
-test2 = reading_json(PATH_JSON)
-print(test)
-print(test2)
