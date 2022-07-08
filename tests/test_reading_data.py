ce149c2b6ad9c9449e784779248637b71633d694
ad50922 <daniel.augusto69@icloud.com>
2022-07-08 11:07:42 +0200

    New tests added

--- tests/test_reading_data.py
+++ tests/test_reading_data.py
@@ -1,6 +1,6 @@
 """In this module we test functionalities of the reading_data module."""
 from unittest import TestCase
-from reading_data import reading_csv
+from reading_data import reading_csv, reading_json
 
 
 class TestReadingCSV(TestCase):
@@ -10,3 +10,12 @@ class TestReadingCSV(TestCase):
         data = reading_csv("test_data.csv")
         self.assertEqual(tuple(data[0]), (1, 2))
         self.assertEqual(tuple(data[1]), (3, 4))
+
+
+class TestReadingJson(TestCase):
+    """Test for json reader"""
+    def test_reading_json(self):
+        """Check behavior of reading_json."""
+        data = reading_json("test_data.json")
+        self.assertTrue(data["dog"])
+        self.assertTrue(data["cat"])
---
+++ tests/test_shaft.py
@@ -0,0 +1,21 @@
+"""In this module we test functionalities of the shaft module."""
+from unittest import TestCase
+from shaft import Shaft
+
+
+class TestShaft(TestCase):
+    """Test for shaft."""
+    def test_calculate(self):
+        """Check behavior of calculate."""
+        shaft1 = Shaft(5, "s232", 4)
+        si_attribute = [0.19634954084936207]
+        shaft1.calculate(10)
+        self.assertEqual(si_attribute, shaft1.si_attribute)
+
+    def test_creating_final_data(self):
+        """Check behavior of creating final data."""
+        shaft1 = Shaft(1231, "1231", 1231)
+        shaft1.si_attribute = [2342, 1232, 21, 1, 3]
+        shaft1.creating_final_data()
+        minimum = min(shaft1.si_attribute)
+        self.assertEqual(minimum, shaft1.result)
